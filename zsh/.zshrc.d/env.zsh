fa77d1c8e562eceb60b43030eb11eca246b66938
Code Slicer <ivanaugustobd@protonmail.com>
2024-01-03 20:11:46 -0300

    v20240103201146

--- zsh/.zshrc.d/env.zsh
+++ zsh/.zshrc.d/env.zsh
@@ -3,9 +3,9 @@ export EDITOR=vim
 export VISUAL=vim
 #export TERM=xterm
 
-# Load sensitive info from an external env file
+# Load extra info from an external env file
 _zsh-load-env-vars() {
-  local ENV_FILE=~/.zsh_env
+  local ENV_FILE=~/.env.zsh
   if [ -e "$ENV_FILE" ]; then source "$ENV_FILE"; fi
 }
 
