29f12e904dcb41df7ef11b235d6e8608db96ac24
Ivan Augusto <4603111+ivanaugustobd@users.noreply.github.com>
2024-01-03 20:01:09 -0300

    v20240103200109

--- zsh/.zshrc.d/env.zsh
+++ zsh/.zshrc.d/env.zsh
@@ -1,3 +1,9 @@
+# Vars
+export EDITOR=vim
+export VISUAL=vim
+#export TERM=xterm
+
+# Load sensitive info from an external env file
 _zsh-load-env-vars() {
   local ENV_FILE=~/.zsh_env
   if [ -e "$ENV_FILE" ]; then source "$ENV_FILE"; fi
