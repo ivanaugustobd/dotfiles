81f36d6a24b5c0f1c1b7de71a2cdb5e2a0cdae8b
Ivan Augusto <4603111+ivanaugustobd@users.noreply.github.com>
2024-01-19 19:45:44 -0300

    v20240119194544

--- zsh/.oh-my-zsh/custom/zsh.zsh
+++ zsh/.oh-my-zsh/custom/zsh.zsh
@@ -2,5 +2,5 @@
 setopt nonomatch # I forgot what this does but I remember I'm used to it already, so keeping that (something about the wildcard glob)
 
 # Aliases
-alias z-config="vi ~/.zshrc"
-alias z-history="vi ~/.zsh_history"
+alias z-config="vim ~/.zshrc"
+alias z-history="vim ~/.zsh_history +:$"
