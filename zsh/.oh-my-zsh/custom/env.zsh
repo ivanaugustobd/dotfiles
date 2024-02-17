00a0f846312faf27fd360c677d1f8114dab268de
Code Slicer <ivanaugustobd@protonmail.com>
2024-02-16 22:20:25 -0300

    v20240216222025

--- zsh/.oh-my-zsh/custom/env.zsh
+++ zsh/.oh-my-zsh/custom/env.zsh
@@ -3,8 +3,6 @@ export EDITOR=vim
 export ELECTRON_TRASH=gio
 export PHP_CS_FIXER_IGNORE_ENV=1
 export VISUAL=vim
-export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
-#export TERM=xterm
 
 # Load extra info from an external env file
 _zsh-load-env-vars() {
